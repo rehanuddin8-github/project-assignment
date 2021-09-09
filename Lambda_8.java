5f1b40908bdb9abe0f936428bb25364a82f178a1
rehanuddin8-github <88127928+rehanuddin8-github@users.noreply.github.com>
2021-09-09 14:08:49 +0530

    Add files via upload

---
+++ Lambda_8.java
@@ -0,0 +1,13 @@
+package lambdaAssignments;
+
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Lambda_8{
+	
+	public static void main(String[] args) {
+		Mythread t= new Mythread();
+		t.start();
+	}
+
+}
